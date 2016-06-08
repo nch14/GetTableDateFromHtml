8fa768bfae8efec3e9086aab235d2d5a525f2474
nch14 <nch14@software.nju.edu.cn>
2016-06-09 03:07:05 +0800

    增加了对更新时间的捕捉

--- src/main/java/Demo/Demo.java
+++ src/main/java/Demo/Demo.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 /**
  * 有一个叫jsoup的东西很好用。通过它、很多时候就不用自己通过正则表达式来解析html文件了
+ * 然后主要需要学会使用的就是Document的select方法！
  * Created by chenh on 2016/6/9.
  */
 public class Demo {
@@ -18,8 +19,13 @@ public class Demo {
 
         Document doc = Jsoup.connect("http://q.10jqka.com.cn/stock/thshy/").get();
 
-        Elements trs = doc.select("table").select("tr");
+        //捕捉到html中的更新时间
+        Elements time=doc.select("div.update_time");
+        String timeString=time.text();
+        System.out.println(timeString);
 
+
+        Elements trs = doc.select("table").select("tr");
         //从1开始是因为一开始瞄了一下，第0行获得的是javascrip的内容
         for(int i = 1;i<trs.size();i++) {
             Elements tds = trs.get(i).select("td");
